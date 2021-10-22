819418f8112f4ba84086c106cb2b3dfb24c0bda5
Ruben Van meerten <rvan-mee@f0r5s12.codam.nl>
2021-10-22 13:52:49 +0200

    fixed ft_memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -9,13 +9,11 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	str = (unsigned char *) s;
 	search = (unsigned char) c;
 	i = 0;
-	while (i < n - 1)
+	while (i < n)
 	{
 		if (str[i] == search)
 			return (&str[i]);
 		i++;
 	}
-	if ((str[i] == search) && (i != n))
-		return (&str[i]);
 	return (0);
 }
